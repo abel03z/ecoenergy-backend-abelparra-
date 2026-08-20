27498f888392c064b9bf4088084cbcb8627f05b8
abel03z <abelfer03z@gmail.com>
2026-08-20 03:09:37 -0400

    feat: agregar app dispositivos con views y urls dinamicas

---
+++ dispositivos/views.py
@@ -0,0 +1,16 @@
+def dispositivos_zona(request, zona_id):
+    if zona_id != 3:
+        return HttpResponse(
+            "Zona no encontrada", status=404
+        )
+    return HttpResponse(
+        f"Dispositivos de la zona {zona_id}"
+    )
+
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse(
+        "<h1>EcoEnergy</h1>"
+        "<p>Back End en funcionamiento</p>"
+    )
