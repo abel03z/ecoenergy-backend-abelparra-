35a997256a781933d6b936645deb4bb4ff13fc49
abel03z <abelfer03z@gmail.com>
2026-09-13 15:53:34 -0300

    Limpia codigo residual en dispositivos/admin.py (pertenecia a devices/admin.py)

--- dispositivos/admin.py
+++ dispositivos/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin, messages
-from django.utils import timezone
 
-
-@admin.action(description="Archivar dispositivos seleccionados", permissions=["change"])
-def archive_dispositivos(modeladmin, request, queryset):
-    updated = queryset.filter(deleted_at__isnull=True).update(deleted_at=timezone.now())
-    modeladmin.message_user(
-        request, f"{updated} dispositivo(s) archivado(s).", level=messages.SUCCESS
-    )
-
-# dentro de DispositivoAdmin:
-class DispositivoAdmin(admin.ModelAdmin):
-    actions = [archive_dispositivos]
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+# Register your models here.
